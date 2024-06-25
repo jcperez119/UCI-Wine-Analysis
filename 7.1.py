c5ef9b4574c8f4e3a8eec544acda46e3f3c47277
jcperez119 <119555375+jcperez119@users.noreply.github.com>
2024-06-24 19:23:44 -0700

    Add files via upload

---
+++ 7.1.py
@@ -0,0 +1,8 @@
+#path to file name
+fpath = "C:/Users/jcper/OneDrive/Desktop/Programming and Continued Education/PY4E/sample_data/words.txt"
+#opens file for reding and printing
+fh = open(fpath)
+#Strips blanks at end of file line, prints line by line in all Upper Case
+for line in fh: 
+    line = line.rstrip()
+    print(line.upper())
